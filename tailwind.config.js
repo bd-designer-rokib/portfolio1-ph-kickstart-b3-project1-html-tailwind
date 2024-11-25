777cc6d56fb2be318699aabcefdf464681f8eeaf
bd-designer-rokib <rokibulhassan1234@gmail.com>
2024-11-26 04:10:48 +0600

    Initial Commit- Not Responsive

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["*"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: "#5E3AEE",
+//         secondary: {
+//           100: "#BFB8B8",
+//           200: "#696969",
+//           300: "#ffffff80",
+//         },
+//       },
+//       fontFamily: {
+//         primary: ["Poppins"],
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
