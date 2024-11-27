3a153ad52d2d2f1b727907d9552540234df42de3
bd-designer-rokib <rokibulhassan1234@gmail.com>
2024-11-27 13:36:27 +0600

    Properly install tailwindcss using postcss and add Poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,12 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["*"],
-//   theme: {
-//     extend: {
-//       colors: {
-//         primary: "#5E3AEE",
-//         secondary: {
-//           100: "#BFB8B8",
-//           200: "#696969",
-//           300: "#ffffff80",
-//         },
-//       },
-//       fontFamily: {
-//         primary: ["Poppins"],
-//       },
-//     },
-//   },
-//   plugins: [],
-// };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
